3347974a7e441e8bdc0db43236df09e41229a046
Dolores <alyciahathaways@gmail.com>
2020-01-27 17:53:23 +0800

    添加 off 方法和测试用例

--- eventhub/src/index.ts
+++ eventhub/src/index.ts
@@ -1,26 +1,36 @@
 class EventHub {
-    cache = {}
+    private cache = {}
     // {
     //     '楚天都市报': [fn1, fn2, fn3],
     //     '羊城晚报': [fn2, fn2, fn3]
     // }
     on(eventName, fn) {
         // 把 fn 推进 this.cache[eventName] 数组
-        if (this.cache[eventName] === undefined) {
-            this.cache[eventName] = []
-        }
+        this.cache[eventName] = this.cache[eventName] || []
         this.cache[eventName].push(fn)
     }
-    emit(eventName) {
+    emit(eventName, data?) {
         // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
-        let array = this.cache[eventName]
-        if (array === undefined) {
-            array = []
-        }
-        array.map(fn => {
-            fn()
-        })
+        (this.cache[eventName] || []).map(fn => fn(data))
+    }
+    off(eventName, fn) {
+        // 把 fn 从 this.cache[eventName] 数组删掉
+        let index = indexOf(this.cache[eventName], 1)
+        if (index === -1) { return }
+        this.cache[eventName].splice(index, 1)
     }
 }
 
 export default EventHub
+
+function indexOf(array, item) {
+    if (array === undefined) { return -1 }
+    let index = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            index = i
+            break
+        }
+    }
+    return index
+}
--- eventhub/test/index.ts
+++ eventhub/test/index.ts
@@ -5,10 +5,22 @@ const eventHub = new EventHub()
 console.assert(eventHub instanceof Object === true, 'eventHub是个对象')
 
 let called = false
-eventHub.on('xxx', ()=> {
+eventHub.on('xxx', (y)=> {
     called = true
-    console.log('被调用')
-    console.log(called)
+    console.log('called:', called)
+    console.assert(y === '今天冠状病毒爆发了')
 })
 
-eventHub.emit('xxx')
+eventHub.emit('xxx', '今天冠状病毒爆发了')
+
+const eventHub2 = new EventHub()
+let called2 = false
+const fn1 = () => {
+    called2 = true
+}
+eventHub2.on('yyy', fn1)
+eventHub2.off('yyy', fn1)
+eventHub2.emit('yyy')
+setTimeout(()=> {
+    console.log(called2)
+}, 1000)
