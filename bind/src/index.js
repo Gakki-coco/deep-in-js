0aaf5c6c161cb7e0eaf9c1993fa55678fd44d8f1
Dolores <alyciahathaways@gmail.com>
2020-02-27 09:55:02 +0800

    初步实现支持 new 的 bind

--- bind/src/index.js
+++ bind/src/index.js
@@ -14,11 +14,17 @@ function bind(asThis) {
     if (typeof fn !== 'function') {
         throw new Error('bind 必须调用在函数上')
     }
-    return function() {
+    return function resultFn() {
         var args2 = slice.call(arguments, 0)
-        return fn.apply(asThis, args.concat(args2))
+        return fn.apply(this.__proto__ === resultFn.prototype ? this : asThis, args.concat(args2))
     }
 }
+
+// new fn() 做了四件事
+// 1. var temp = {}
+// 2. temp.__proto__ = fn.prototype
+// 3. fn.call(temp, 'x')
+// 4. fn 函数体里 return this
 
 module.exports = bind
 
--- bind/test/index.js
+++ bind/test/index.js
@@ -1,25 +1,61 @@
 const bind  = require('../src/index')
 
-Function.prototype.bind2 = bind
-console.assert(Function.prototype.bind2 !== undefined)
+test1('fn.bind 能用')
+test2('this 绑定成功')
+test3('this, p1, p2 绑定成功')
+test4('this, p1 绑定成功, 后传 p2 调用成功')
+test5('new 的时候绑定了 p1, p2')
 
-const fn1 = function() {
-    return this
-}
-const newFn1 = fn1.bind2({name: 'Gakki'})
-console.assert(newFn1().name === 'Gakki')
-
-
-const fn2 = function(param1, param2) {
-    return [this, param1, param2]
+function test1(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    console.assert(Function.prototype.bind2 !== undefined)
 }
 
-const newFn2 = fn2.bind2({name: 'Gakki'}, 123, 456)
-console.assert(newFn2()[0].name === 'Gakki', 'this')
-console.assert(newFn2()[1] === 123, 'param1')
-console.assert(newFn2()[2] === 456, 'param2')
+function test2(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn1 = function () {
+        return this
+    }
+    const newFn1 = fn1.bind2({ name: 'Gakki' })
+    console.assert(newFn1().name === 'Gakki')
+}
 
-const anotherFn2 = fn2.bind2({name: 'Gakki'}, 111)
-console.assert(anotherFn2(222)[0].name === 'Gakki', 'anotherThis')
-console.assert(anotherFn2(222)[1] === 111, 'param111')
-console.assert(anotherFn2(222)[2] === 222, 'param222')
+function test3(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn2 = function (param1, param2) {
+        return [this, param1, param2]
+    }
+    const newFn2 = fn2.bind2({ name: 'Gakki' }, 123, 456)
+    console.assert(newFn2()[0].name === 'Gakki', 'this')
+    console.assert(newFn2()[1] === 123, 'param1')
+    console.assert(newFn2()[2] === 456, 'param2')
+}
+
+function test4(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn2 = function (param1, param2) {
+        return [this, param1, param2]
+    }
+    const anotherFn2 = fn2.bind2({ name: 'Gakki' }, 111)
+    console.assert(anotherFn2(222)[0].name === 'Gakki', 'anotherThis')
+    console.assert(anotherFn2(222)[1] === 111, 'param111')
+    console.assert(anotherFn2(222)[2] === 222, 'param222')
+}
+
+function test5(message) {
+    console.log(message)
+    Function.prototype.bind2 = bind
+    const fn = function (param1, param2) {
+        this.param1 = param1
+        this.param2 = param2
+    }
+    const fn2 = fn.bind2(undefined, 'x', 'y')
+    const object = new fn2()
+    console.log(object)
+    console.assert(object.param1 === 'x', 'x')
+    console.assert(object.param2 === 'y', 'y')
+}
